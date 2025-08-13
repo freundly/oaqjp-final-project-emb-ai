99aa8fa262d1acbceb9066ea4aa053dcb0c859a1
freundly <berkesoyturk@hotmail.com>
2025-08-13 11:48:46 -0400

    d

--- EmotionDetection/__init__.py
+++ EmotionDetection/__init__.py
@@ -1,3 +1,5 @@
-# EmotionDetection/__init__.py
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))  # final_project klasörünü path’e ekle
 
-from .emotion_detection import emotion_detector
+from emotion_detection import emotion_detector
